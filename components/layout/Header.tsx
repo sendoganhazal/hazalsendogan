e97d4d20ad8719b91d4265acc4d91a0272898fa3
Hazal Sendogan <sendoganhazal@gmail.com>
2025-12-11 13:36:59 +0300

    header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -26,29 +26,29 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const navLinks: NavLinksProps[] = headerData.navLinks;
   return (
-    <header className="bg-zinc-50">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+    <header className="header">
+      <nav aria-label="Global" className="navbar">
+        <div className="navbar-brand-lg">
+          <Link href="/" className="-m-1.5 p-1.5">
             <Image
               width={100} height={100}
               alt=""
               src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
+              className="navbar-logo"
             />
-          </a>
+          </Link>
         </div>
-        <div className="flex lg:hidden">
+        <div className="mobile-menu-wrapper">
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="mobile-menu-btn"
           >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="size-6" />
           </button>
         </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+        <PopoverGroup className="popover-group">
           {
             navLinks.map((link) => (
               <Link key={link.id} href={link.slug} className='nav-link popover-link'>{link.label}</Link>
@@ -58,30 +58,30 @@ export default function Header() {
         </PopoverGroup>
 
       </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="dialog">
         <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+        <DialogPanel className="dialog-panel">
+          <div className="dialog-header">
+            <Link href="#" className="-m-1.5 p-1.5">
               <Image
                 width={100} height={100}
                 alt=""
                 src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
+                className="dialog-logo"
               />
-            </a>
+            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="dialog-close-btn"
             >
               <span className="sr-only">Close menu</span>
               <XMarkIcon aria-hidden="true" className="size-6" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
+          <div className="dialog-nav-root">
+            <div className="dialog-nav-divide">
+              <div className="dialog-nav">
                 {
                   navLinks.map((link) => (
                     <Link key={link.id} href={link.slug} className='nav-link dialog-link'>{link.label}</Link>
