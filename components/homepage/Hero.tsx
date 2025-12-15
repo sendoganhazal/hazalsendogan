bc7820bac5711c4b87e55cbd9c25c24b2b0edfb2
Hazal Sendogan <sendoganhazal@gmail.com>
2025-12-15 15:13:57 +0300

    header was revised

--- components/homepage/Hero.tsx
+++ components/homepage/Hero.tsx
@@ -6,8 +6,9 @@ function Hero() {
       <div className='hero-content-wrapper'>
         <div className='hero-content'>
           <h1>Hazal Şendoğan</h1>
-          <h2>Frontend Developer</h2>
+          <h2>Frontend Developer, React.js & Next.js Specialist, Business Analyst, UI & UX Advisor</h2>
           <p>I am a Frontend Developer specialized in React.js and Next.js, passionate about building modern, user-friendly, and high-performance web applications.</p>
+          <p>For eight years, I participated in numerous projects in the software industry, holding roles such as Frontend Developer, Business Analyst, IT Consultant, and UX & UI Advisor. </p>
         </div>
         <div className='hero-image'>
           
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -23,13 +23,16 @@ interface NavLinksProps {
 
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
   const navLinks: NavLinksProps[] = headerData.navLinks;
   return (
     <header className="header">
       <nav aria-label="Global" className="navbar">
         <div className="navbar-brand-lg">
-          <Link href="/" className="-m-1.5 p-1.5">
+          <Link href="/" className="p-2">
             <Image
               width={100} height={100}
               alt=""
@@ -38,61 +41,96 @@ export default function Header() {
             />
           </Link>
         </div>
-        <div className="mobile-menu-wrapper">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="mobile-menu-btn"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
-          </button>
+
+        {/*Desktop Menu */}
+        <div className='navbar-nav'>
+          <ul className="nav-list">
+            {
+              navLinks.map((link) => (
+                <li className="nav-list-item" key={link.id}>
+                  <Link href={link.slug} className='nav-link'>{link.label}</Link>
+                </li>
+
+              ))
+            }
+
+          </ul>
         </div>
-        <PopoverGroup className="popover-group">
-          {
-            navLinks.map((link) => (
-              <Link key={link.id} href={link.slug} className='nav-link popover-link'>{link.label}</Link>
-            ))
-          }
-
-        </PopoverGroup>
-
-      </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="dialog">
-        <div className="fixed inset-0 z-50" />
-        <DialogPanel className="dialog-panel">
-          <div className="dialog-header">
-            <Link href="#" className="-m-1.5 p-1.5">
-              <Image
-                width={100} height={100}
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="dialog-logo"
-              />
-            </Link>
+        {/* Mobile Menu */}
+        <div className='mobile-menu-wrapper'>
+          <div className="lg:hidden">
             <button
+              className="mobile-menu-btn"
+              onClick={toggleMobileMenu}
               type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="dialog-close-btn"
             >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
+              <span className="mobile-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </span>
             </button>
           </div>
-          <div className="dialog-nav-root">
-            <div className="dialog-nav-divide">
-              <div className="dialog-nav">
-                {
-                  navLinks.map((link) => (
-                    <Link key={link.id} href={link.slug} className='nav-link dialog-link'>{link.label}</Link>
-                  ))
-                }
 
+          <div
+            className={`mobile-nav ${isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"}`}
+          >
+            <div className="mobile-menu-header">
+              <div className="navbar-brand">
+                <Link href="/" className=" p-2">
+                  <Image
+                    width={100} height={100}
+                    alt=""
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                    className="navbar-logo"
+                  />
+                </Link>
               </div>
+              <button
+                onClick={toggleMobileMenu}
+                className="close-btn"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-10 h-10 block"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
             </div>
+            <ul className="nav-list">
+              {
+                navLinks.map((link) => (
+                  <li className="nav-list-item" key={link.id}>
+                    <Link href={link.slug} className='nav-link'>{link.label}</Link>
+                  </li>
+
+                ))
+              }
+
+            </ul>
           </div>
-        </DialogPanel>
-      </Dialog>
+        </div>
+
+
+      </nav>
     </header>
   )
 }
